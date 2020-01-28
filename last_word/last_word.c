0bf395ca60971ea92c1889993f5ee14413f6b012
Tyrelle Baatjies <tbaatjie@c4r16s10.wethinkcode.co.za>
2020-01-28 12:05:16 +0200

    exam

---
+++ last_word/last_word.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   last_word.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaatjie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/28 08:16:09 by tbaatjie          #+#    #+#             */
+/*   Updated: 2020/01/28 10:03:36 by tbaatjie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	if(argc == 2)
+	{
+		while(argv[1][i] != '\0')							// Goes to end of string
+			i++;		
+		i--;
+
+		while(argv[1][i] == ' ' || argv[1][i] == '\t')		// Goes past spaces at end of string if any
+			i--;
+
+		while(!(argv[1][i] == ' ' || argv[1][i] == '\t'))	// At last char of last word, goes to before first char
+			i--; 		
+		i++;												// Moves on to first char
+
+		while(!(argv[1][i] == ' ' || argv[1][i] == '\t') && argv[1][i] != '\0')	// Moves ++ until space or nullterm,
+		{
+			write(1, &argv[1][i], 1);
+			i++; 
+		}
+
+
+
+	}
+	write(1, "\n" ,1);
+	return (0);
+ 
